7af78f8dd8da5840d3079cf786c6e58eb518177b
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-06-14 10:59:45 +0530

    job applying project - selenium webdriver | webscraping (LinkedIn)

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options=options)
+
+driver.get('https://www.linkedin.com/jobs/search')
+
+signin_button = driver.find_element(By.LINK_TEXT, 'Sign in')
+signin_button.click()
+
+email = driver.find_element(By.NAME, 'session_key')
+email.send_keys(os.environ.get('EMAIL'))
+email.send_keys(Keys.TAB)
+password = driver.find_element(By.NAME, 'session_password')
+password.send_keys(os.environ.get('PASSWORD'))
+signin = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+signin.click()
+
+time.sleep(2)
+
+all_jobs = driver.find_element(By.CSS_SELECTOR, ".job-card-container--clickable")
+
+for job in all_jobs:
+    time.sleep(2)
+    job.click()
+
+    try:
+        apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+        apply_button.click()
+        time.sleep(5)
+
+        phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
+        if phone.text == "":
+            phone.send_keys(os.environ.get('PHONE'))
+
+        time.sleep(5)
+        submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
+        submit_button.click()
+
+        close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+        close_button.click()
+
+    except NoSuchElementException:
+        print("Button element not found.")
+        continue
+
+driver.quit()
+
