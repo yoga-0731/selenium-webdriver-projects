d99d22dc3065bc15067003cf14240031445e3fba
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-06-16 11:31:28 +0530

    selenium webdriver - click, type, search, press keys

--- main.py
+++ main.py
@@ -1,54 +1,46 @@
-import os
-import time
+# https://github.com/yoga-0731/amazon-price-tracker-bot/
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 
+# Preventing browser from closing
 options = webdriver.ChromeOptions()
-options.add_experimental_option('detach', True)  # Added to prevent closing of browser
-driver = webdriver.Chrome(options=options)  # Creating object
+options.add_experimental_option("detach", True)
 
-driver.get('https://www.linkedin.com/jobs/search')
+# Create object
+driver = webdriver.Chrome(options=options)
 
-signin_button = driver.find_element(By.LINK_TEXT, 'Sign in')
-signin_button.click()
+driver.get("https://www.amazon.in/AGARO-Utensils-Stainless-Resistant-Cookware/dp/B09XTYCLDJ/ref=sr_1_6?crid=1AYIA3HNZ6HL2&keywords=kitchen+utensils&qid=1686540620&sprefix=kitchen+utensil%2Caps%2C421&sr=8-6")
+price = driver.find_element(By.CLASS_NAME, "a-price-whole")
+print(price.text)
 
-email = driver.find_element(By.NAME, 'session_key')
-email.send_keys(os.environ.get('EMAIL'))  # Typing
-email.send_keys(Keys.TAB)
-password = driver.find_element(By.NAME, 'session_password')
-password.send_keys(os.environ.get('PASSWORD'))
-signin = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-signin.click()
+price_through_xpath = driver.find_element(By.XPATH, '//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[2]/span[2]/span[2]')
+print(price_through_xpath.text)
 
-time.sleep(2)  # Sleep added to give time for searching elements, else we might have NoSuchElementException
 
-all_jobs = driver.find_element(By.CSS_SELECTOR, ".job-card-container--clickable")
+# Challenge - 1: Getting Upcoming Events from python.org page
+driver.get('https://www.python.org')
 
-for job in all_jobs:
-    time.sleep(2)
-    job.click()
+upcoming_events = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[2]/div[2]/div/ul').text.split('\n')
+events = {upcoming_events[i]: upcoming_events[i+1] for i in range(0, len(upcoming_events), 2)}
+print(events)
 
-    try:
-        apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
-        apply_button.click()
-        time.sleep(5)
+# Challenge - 2: Getting Wikipedia data
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+count = driver.find_element(By.XPATH, '//*[@id="articlecount"]/a[1]')
+article_count = count[0].text
+print(article_count)
 
-        phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
-        if phone.text == "":
-            phone.send_keys(os.environ.get('PHONE'))
+# Clicking
+count[0].click()
+# (or)
+count = driver.find_element(By.LINK_TEXT, str(article_count))
+count.click()
 
-        time.sleep(5)
-        submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
-        submit_button.click()
-
-        close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
-        close_button.click()
-
-    except NoSuchElementException:
-        print("Button element not found.")
-        continue
+# Typing/Searching and hitting Enter
+search_bar = driver.find_element(By.NAME, 'search')
+search_bar.send_keys('Python')
+search_bar.send_keys(Keys.ENTER)
 
 driver.quit()
-
